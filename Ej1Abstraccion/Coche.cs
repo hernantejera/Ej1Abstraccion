9adc63f58dd1de3a02df27b45340398b67b1ba80
hernantejera <hernanmtejera@gmail.com>
2023-07-15 11:38:44 -0300

    se realizo la tarea

--- Ej1Abstraccion/Coche.cs
+++ Ej1Abstraccion/Coche.cs
@@ -8,6 +8,21 @@ namespace Ej1Abstraccion
 {
     public class Coche : MedioTransporte
     {
+        //Constructor por defecto (Todas las clases lo tienen pero no se ven )
+        public Coche() 
+        {
+        }
+        //Constructor con un parametro de tipo entero para definir el año de fabricacion 
+        public Coche(int anioFabricacion) 
+        {
+        AnioFabricacion = anioFabricacion;
+        }
+        //Constructor con dos parametros de tipo entero para definir el año de fabricacion y capacidad
+        public Coche(int anioFabricacion,int capacidad) 
+        {
+            AnioFabricacion = anioFabricacion;
+            Capacidad = capacidad;
+        }
         public override string Arrancar()
         {
             return "Enciendo el motor y salgo ";
--- Ej1Abstraccion/Program.cs
+++ Ej1Abstraccion/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-           
+            //Inicializamos con el constructor dentro de la clase choche 
+            // donde recibe los dos parametros para el año de fabricacion y capacidad 
+           Coche  coche = new Coche(1990,5);
+
+            Avion avion = new Avion()
+            {
+                AnioFabricacion = 2005,
+                Capacidad = 200
+            };
+            Barco barco = new Barco()
+            {
+                AnioFabricacion = 2000,
+            Capacidad = 500
+            };
+            var arrancarCoche = coche.Arrancar();
+            var detenerCoche = coche.Detener();
+
+            var arrancarBarco = barco.Arrancar();
+            var detenerBarco = barco.Detener();
+
+            var arrancarAvion = avion.Arrancar();
+            var detenerAvion = avion.Detener(); 
+
+
+            Console.WriteLine($"El coche arranca :{arrancarCoche}");
+            Console.WriteLine($"El coche se detiene:{detenerCoche}");
+
+            Console.WriteLine($"El barco arranca:{arrancarBarco}");
+            Console.WriteLine($"El barco se detiene:{detenerBarco}");
+
+            Console.WriteLine($"El avion arranca:{arrancarAvion}");
+            Console.WriteLine($"El avion se detiene:{detenerAvion}");
         }
     }
 }
